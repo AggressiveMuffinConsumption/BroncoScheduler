8eedbd9f0ca7c168c6804c96b37a7156762fa2bf
Ricky <ricky.y.fok@gmail.com>
2022-02-23 10:53:15 -0800

    ricky url

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('hello/', hello.views.say_hello),
     path('johnathan/', hello.views.johnathan),
+    path('ricky/', hello.views.ricky),
 ]
