7763250519b32727504d444153bafbdd5bb975c6
johnathangiang <76720038+johnathangiang@users.noreply.github.com>
2022-02-22 23:31:59 -0800

    johnathan url

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,20 @@
+import requests
+
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    r = requests.get('https://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
+def say_hello(request):
+    return render(request, 'hello.html')
+
+def johnathan(request):
+    return render(request, 'johnathan.html')
 
 def db(request):
 
