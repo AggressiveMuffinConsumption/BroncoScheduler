b7d1b28aed938397e4a2c9f74deaecf378938538
April Trinh <aprild2214@gmail.com>
2022-03-06 12:14:08 -0800

    added vue components

---
+++ GUI/djackets_vue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <LogIn msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LogIn from '@/components/LogIn.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LogIn
+  }
+}
+</script>
