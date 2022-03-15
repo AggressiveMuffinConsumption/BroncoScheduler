885bd6afd088503082851fc574c4b1fc2aa653fc
April Trinh <aprild2214@gmail.com>
2022-03-14 22:05:50 -0700

    Update views.py

--- API/hello/views.py
+++ API/hello/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from .models import Greeting
 from scipy import special
 import pandas
+import scrapy
+import unittest
 
 # Create your views here.
 def index(request):
@@ -15,7 +17,7 @@ def index(request):
 def say_hello(request):
     return render(request, 'hello.html')
 
-def johnathgitan(request):
+def johnathan(request):
     return render(request, 'johnathan.html')
 
 def ricky(request):
@@ -43,6 +45,17 @@ def db(request):
     return render(request, "db.html", {"greetings": greetings})
 
 def matrix(request):
+    aString, bString ,cString ,c1String = matrixUse()
+    context = {
+        "matrix_a": aString,
+        "matrix_b": bString,
+        "matrix_c": cString,
+        "matrix_c1": c1String
+    }
+    template_name="numpy.html"
+    return render(request, template_name, context)
+
+def matrixUse():
     a = numpy.array([[1,2,3],[4,5,6],[7,8,9]])
     b = numpy.array([2,2,2])
     c = a * b #element wise multiplication
@@ -54,16 +67,9 @@ def matrix(request):
     bString = numpy.array2string(b)
     cString = numpy.array2string(c)
     c1String = numpy.array2string(c1)
-    context = {
-        "matrix_a": aString,
-        "matrix_b": bString,
-        "matrix_c": cString,
-        "matrix_c1": c1String
-    }
-    template_name="numpy.html"
-    return render(request, template_name, context)
+    return aString,bString,cString,c1String
 
-def panda(request) :
+def pandas(request) :
     names = ["Peter", "Paul", "Tracy", "Trucy"]
     years = [3, 2, 4, 1]
     majors = ["Computer Science", "Computer Information System", "Computer Engineering", "Undeclared"]
@@ -75,3 +81,17 @@ def panda(request) :
     context = {test2}
     template_name = "pandas.html"
     return render(request, template_name, context)
+
+class MySpider(scrapy.Spider):
+    name = 'testspider'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['https:quotes.toscrape.com/']
+
+    def parse(self, response):
+        quotes = response.xpath("//div[@class='quote']").extract()
+        yield{'quotes': quotes}
+
+def scrapy(request):
+    contexts = {quotes}
+    template_name = "scrapy.html"
+    return render(request, template_name, contexts)
